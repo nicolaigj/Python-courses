c08bb7dd6a28ebb2ab442ea5d756cc91a6217a1e
nicolaigj <ngjellestad@gmail.com>
2016-04-25 22:31:02 +0200

    Add last program of chapter 4

---
+++ Book1.Python.Crash.Course/Chapter_4/4_13_buffet.py
@@ -0,0 +1,7 @@
+basic_foods = ('pizza', 'mac \'n cheese', 'horseradish', 'apple', 'ice cream')
+for food in basic_foods:
+    print(food)
+
+basic_foods = ('pizza', 'mac \'n cheese', 'horseradish', 'onion soup', 'tapas')
+for food in basic_foods:
+    print(food)
